762b7a399e4cc535dd5c7ce5ec5cecb0f64c2880
josdugan <josdugan@gmail.com>
2022-01-08 20:52:37 -0500

    Add auth flow

--- server/index.js
+++ server/index.js
@@ -1,11 +1,46 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/user');
+require('./services/passport');
+const authRoutes = require('./routes/authRoutes');
+const keys = require('./config/keys');
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(PORT);
+app.use(authRoutes);
+
+const start = () => {
+  if (!process.env.GOOGLE_CLIENT_ID) {
+    console.log('GOOGLE_CLIENT_ID must be set');
+  }
+  if (!process.env.GOOGLE_CLIENT_SECRET) {
+    console.log('GOOGLE_CLIENT_SECRET must be set');
+  }
+  if (!process.env.MONGO_URI) {
+    console.log('MONGO_URI must be set');
+  }
+  if (!process.env.COOKIE_KEY) {
+    console.log('COOKIE_KEY must be set');
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+  });
+};
+
+start();
